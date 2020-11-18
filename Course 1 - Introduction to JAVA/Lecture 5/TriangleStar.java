f5b19da6ab12eff56ecef843fb6fb8d49a481584
Akshaya Balaji <37283561+akshayavb99@users.noreply.github.com>
2020-11-18 12:01:38 +0530

    Create TriangleStar.java

---
+++ Course 1 - Introduction to JAVA/Lecture 5/TriangleStar.java
@@ -0,0 +1,63 @@
+/*
+Print the following pattern for the given N number of rows.
+Pattern for N = 4
+*
+**
+***
+****
+
+Note : There are no spaces between the stars (*).
+
+Input format :
+Integer N (Total no. of rows)
+
+Output format :
+Pattern in N lines
+
+Constraints
+0 <= N <= 50
+
+Sample Input 1:
+5
+Sample Output 1:
+*
+**
+***
+****
+*****
+
+Sample Input 2:
+6
+Sample Output 2:
+*
+**
+***
+****
+*****
+******
+*/
+
+import java.util.*;
+public class TriangleStar {
+
+
+	public static void main(String[] args) {
+		
+		/* Your class should be named Solution.
+	 	* Read input as specified in the question.
+	 	* Print output as specified in the question.
+		*/
+		Scanner scan=new Scanner(System.in);
+        int N=scan.nextInt();
+        for(int i=0;i<N;i++)
+        {
+            for(int j=0;j<=i;j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        }
+		    scan.close();
+	}
+
+}
