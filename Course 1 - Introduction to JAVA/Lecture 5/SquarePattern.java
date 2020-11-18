cd53d5db6fe8dda5b72b5315e66e8ae6b61a05af
Akshaya Balaji <37283561+akshayavb99@users.noreply.github.com>
2020-11-18 12:00:12 +0530

    Create SquarePattern.java

---
+++ Course 1 - Introduction to JAVA/Lecture 5/SquarePattern.java
@@ -0,0 +1,65 @@
+/*
+Print the following pattern for the given N number of rows.
+
+Pattern for N = 4
+4444
+4444
+4444
+4444
+
+Input format :
+Integer N (Total no. of rows)
+
+Output format :
+Pattern in N lines
+
+Constraints
+0 <= N <= 50
+
+Sample Input 1:
+7
+Sample Output 1:
+7777777
+7777777
+7777777
+7777777
+7777777
+7777777
+7777777
+
+Sample Input 2:
+6
+Sample Output 2:
+666666
+666666
+666666
+666666
+666666
+666666
+*/
+
+import java.util.*;
+public class SquarePattern {
+
+
+	public static void main(String[] args) {
+		
+		/* Your class should be named Solution.
+	 	* Read input as specified in the question.
+	 	* Print output as specified in the question.
+		*/
+		Scanner scan = new Scanner(System.in);
+        int n=scan.nextInt();
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                System.out.print(n);
+            }
+            System.out.println("");
+        }
+        scan.close();
+		
+	}
+
+}
