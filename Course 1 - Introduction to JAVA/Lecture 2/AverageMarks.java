8b69b9e29088dc5d331e930fe55828ee8989817f
Akshaya Balaji <37283561+akshayavb99@users.noreply.github.com>
2020-11-18 12:06:16 +0530

    Update AverageMarks.java

--- Course 1 - Introduction to JAVA/Lecture 2/AverageMarks.java
+++ Course 1 - Introduction to JAVA/Lecture 2/AverageMarks.java
@@ -35,13 +35,8 @@ public class AverageMarks {
 
 
 	public static void main(String[] args) {
-		
-		/* Your class should be named Solution.
-	 	* Read input as specified in the question.
-	 	* Print output as specified in the question.
-		*/
 
-		Scanner scan = new Scanner(System.in);
+	Scanner scan = new Scanner(System.in);
         String name;
         int m1,m2,m3;
         name=scan.next();
