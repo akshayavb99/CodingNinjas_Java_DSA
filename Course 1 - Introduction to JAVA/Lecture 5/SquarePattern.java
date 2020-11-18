db9683a03b697c61aba905c039186bd0876e6a47
Akshaya Balaji <37283561+akshayavb99@users.noreply.github.com>
2020-11-18 12:04:34 +0530

    Update SquarePattern.java

--- Course 1 - Introduction to JAVA/Lecture 5/SquarePattern.java
+++ Course 1 - Introduction to JAVA/Lecture 5/SquarePattern.java
@@ -43,11 +43,6 @@ public class SquarePattern {
 
 
 	public static void main(String[] args) {
-		
-		/* Your class should be named Solution.
-	 	* Read input as specified in the question.
-	 	* Print output as specified in the question.
-		*/
 		Scanner scan = new Scanner(System.in);
         int n=scan.nextInt();
         for(int i=0;i<n;i++)
