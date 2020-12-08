cc40d67af36dca261e4b75bd25345e9e4d08c9c4
Akshaya Balaji <37283561+akshayavb99@users.noreply.github.com>
2020-12-08 12:13:53 +0530

    Create LengthOfLL.java

---
+++ Course 2 - Data Structures in JAVA/Lecture 7 - Linked Lists I/LengthOfLL.java
@@ -0,0 +1,61 @@
+/*
+For a given singly linked list of integers, find and return its length. Do it using an iterative method.
+Input format :
+The first line contains an Integer 't' which denotes the number of test cases or queries to be run. Then the test cases follow.
+First and the only line of each test case or query contains elements of the singly linked list separated by a single space. 
+Remember/Consider : While specifying the list elements for input, -1 indicates the end of the singly linked list and hence, would never be a list element.
+
+Output format :
+For each test case, print the length of the linked list.
+Output for every test case will be printed in a separate line.
+
+Constraints :
+1 <= t <= 10^2
+0 <= N <= 10^5
+Time Limit: 1 sec
+
+Sample Input 1 :
+1
+3 4 5 2 6 1 9 -1
+Sample Output 1 :
+7
+
+Sample Input 2 :
+2
+10 76 39 -3 2 9 -23 9 -1
+-1
+Sample Output 2 :
+8
+0
+*/
+
+/*
+
+ 	Following is the Node class already written for the Linked List
+
+	class LinkedListNode<T> {
+		T data;
+		LinkedListNode<T> next;
+
+		public Node(T data) {
+			this.data = data;
+		}
+	}
+
+*/
+
+
+public class LengthOfLL {
+
+	public static int length(LinkedListNode<Integer> head){
+		//Your code goes here
+        LinkedListNode<Integer> n=head;
+        int count=0;
+        while(n!=null)
+        {
+            count++;
+            n=n.next;
+        }
+        return count;
+	}
+}
